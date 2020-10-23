d2bd0cf8ddc8416d16dca17000803789c3ac9692
smuts101 <69514780+smuts101@users.noreply.github.com>
2020-10-23 09:43:00 +0200

    converted from angular to ionic and connected to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import * as firebase from 'firebase/app';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCasNI2qAoo17xvXOHM1rF3v9ARz5pdwaA",
+  authDomain: "shopping-list-2e9f6.firebaseapp.com",
+  databaseURL: "https://shopping-list-2e9f6.firebaseio.com",
+  projectId: "shopping-list-2e9f6",
+  storageBucket: "shopping-list-2e9f6.appspot.com",
+  messagingSenderId: "436891313869",
+  appId: "1:436891313869:web:9dd11985ec5a0a3829eb84",
+  measurementId: "G-95DX6L2ZW1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
